08d151765a941785a4d807d3782ab8ed3de7cf06
arameshraju <alluri.ramesh@gmail.com>
2020-07-12 09:28:52 +0530

    Bottomnav

---
+++ lib/BottomNav.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class bottomRoot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Ramesh App",
+      home: ,
+    )
+  }
+}
---
+++ lib/BottomNavHome.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomNaveHome extends StatefulWidget {
+  @override
+  _BottomNaveHomeState createState() => _BottomNaveHomeState();
+}
+
+class _BottomNaveHomeState extends State<BottomNaveHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: "Ramesh",
+        ),
+
+    )
+  }
+}
